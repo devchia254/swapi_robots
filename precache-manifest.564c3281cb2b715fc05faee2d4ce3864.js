b4d1f679ec37f0dcfe236585c8bddd8df2ddaf6f
devchia254 <alex7chiaster@gmail.com>
2019-05-25 16:12:23 +0800

    Updates

---
+++ precache-manifest.564c3281cb2b715fc05faee2d4ce3864.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebad8271135036480050773202ea82c8",
+    "url": "/swapi_robots/index.html"
+  },
+  {
+    "revision": "f39a643f8ecf15d2111c",
+    "url": "/swapi_robots/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4709b67bad853eedecfc",
+    "url": "/swapi_robots/static/css/main.06229612.chunk.css"
+  },
+  {
+    "revision": "f39a643f8ecf15d2111c",
+    "url": "/swapi_robots/static/js/2.9dade313.chunk.js"
+  },
+  {
+    "revision": "4709b67bad853eedecfc",
+    "url": "/swapi_robots/static/js/main.bf9bedc1.chunk.js"
+  },
+  {
+    "revision": "bcb26fc79472f3864b26",
+    "url": "/swapi_robots/static/js/runtime~main.53c5c576.js"
+  },
+  {
+    "revision": "02980b307fdf1ec3fce5f53eaa190405",
+    "url": "/swapi_robots/static/media/Starjedi.02980b30.ttf"
+  },
+  {
+    "revision": "03f961de17b2e89cb5d80008546dd058",
+    "url": "/swapi_robots/static/media/Starjedi.03f961de.svg"
+  },
+  {
+    "revision": "6a5d63cff0c43a9294b5d91bf53d2845",
+    "url": "/swapi_robots/static/media/Starjedi.6a5d63cf.woff"
+  },
+  {
+    "revision": "7248a9a5e066780f6f8d42658b1be6c4",
+    "url": "/swapi_robots/static/media/Starjedi.7248a9a5.eot"
+  },
+  {
+    "revision": "ee7632779ceff1dbbec4bc7309aca5ee",
+    "url": "/swapi_robots/static/media/starwars_bg.ee763277.jpg"
+  }
+]);
